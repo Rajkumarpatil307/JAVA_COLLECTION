68d8bc695067fc8507cf76f5abf11eeb9301fbcb
Rajkumarpatil307 <patil.rajkumar307@gmail.com>
2024-08-09 15:43:58 +0530

    The collection Class are commited iontial

---
+++ HashSetExample.java
@@ -0,0 +1,10 @@
+package Java_concepts;
+
+public class HashSetExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ StackExample.java
@@ -0,0 +1,10 @@
+package Java_concepts;
+
+public class StackExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
